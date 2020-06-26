5165c664371ae6fdcfc1ae783460f006c8385935
nax2uk <azlinayeo@gmail.com>
2020-06-26 15:47:49 +0100

    added hrefs to social media icons in footer

--- src/Navs/Footer.jsx
+++ src/Navs/Footer.jsx
@@ -5,10 +5,17 @@ const Footer = () => {
   return (
     <footer className="footer bg-dark">
       <div className="social">
-        <a href="/"><i className="fab fa-facebook fa-2x"></i></a>
-        <a href="/"><i className="fab fa-twitter fa-2x"></i></a>
-        <a href="/"><i className="fab fa-youtube fa-2x"></i></a>
-        <a href="/"><i className="fab fa-linkedin fa-2x"></i></a>
+        <a href="https://www.facebook.com/northcoders/" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook fa-2x"></i>
+        </a>
+        <a href="https://twitter.com/northcoders" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter fa-2x"></i>
+        </a>
+        <a href="https://www.youtube.com/northcoders" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-youtube fa-2x"></i></a>
+        <a href="https://www.linkedin.com/company/northcoders/" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
       </div>
       <p>Copyright &copy; {year} - Northcoders</p>
     </footer>
